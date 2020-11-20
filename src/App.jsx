73fc3db770b91a823eb96bb4e127a7b87e09fd1e
Paulo Henrique <paulo.scape@gmail.com>
2020-11-20 14:01:40 -0300

    update App to show Modal

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./index.css";
+import Modal from "./Modal";
 
-const App = () => <div>Hi there, I'm React from Webpack 5.</div>;
+const App = () => (
+  <div>
+    <div>Hi there, I'm the host of the {`Modal`} Component.</div>
+    <Modal>WoooT</Modal>
+  </div>
+);
 
 ReactDOM.render(<App />, document.getElementById("app"));

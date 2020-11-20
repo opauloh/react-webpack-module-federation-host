70d03f4dda911a1ddd5ce19e7f8ca4db4f60edaf
Paulo Henrique <paulo.scape@gmail.com>
2020-11-20 14:11:37 -0300

    add production to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,12 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 const deps = require("./package.json").dependencies;
-module.exports = {
+module.exports = (_, argv) => ({
   output: {
-    publicPath: "http://localhost:8080/",
+    publicPath:
+      argv.mode === "development"
+        ? "http://localhost:8080/"
+        : "https://react-webpack-module-federation-host.vercel.app/",
   },
 
   resolve: {
@@ -60,4 +63,4 @@ module.exports = {
       template: "./src/index.html",
     }),
   ],
-};
+});
